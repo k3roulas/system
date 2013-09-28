d6def17d6aa87e1da691b7748e54e93ff4af726d
k3roulas <k3roulas@gmail.com>
2013-09-28 13:35:28 +0100

    enhance cp script

--- cp.sh
+++ cp.sh
@@ -1,5 +1,30 @@
-cp ~/.bashrc .
-cp ~/.vimrc .
-cp -R ~/.vim .
-cp -R ~/.zshrc .
-cp ~/.tmux.conf .
+#!/bin/ksh
+if [ $# -ne "1" ] || [ "$1" != "install"  ] && [ "$1" != "save"  ]
+then
+    echo "usage :"
+    echo "   $0 install | save "
+    exit
+fi
+
+if [ "$1" == "install" ]
+then
+    SOURCE=./
+    DEST=~/
+else
+    SOURCE=~/
+    DEST=./
+fi
+
+set -A LISTFILE .bashrc .vimrc .zshrc .tmux.conf
+set -A LISTDIR  .vim
+
+for I in ${LISTFILE[@]}
+do
+    cp $SOURCE$I $DEST$I
+done
+
+for I in ${LISTDIR[@]}
+do
+    cp -R $SOURCE$I $DEST$I
+done
+
